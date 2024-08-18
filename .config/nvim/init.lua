23fd6125f403570dc8fddfb351fdc8a67ad69c50
Myeongseok Baek <codetemplate@hanmail.net>
2024-08-18 10:42:08 +0900

    2024-08-18

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,8 +15,8 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<M-c>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+-- vim.g.copilot_no_tab_map = true
+-- vim.api.nvim_set_keymap("i", "<M-c>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 require "lazy_setup"
 require "polish"
