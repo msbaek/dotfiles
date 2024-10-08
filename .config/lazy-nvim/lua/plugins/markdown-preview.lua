adfb154a8e9248b9318a86e71e88714478bcdcda
Myeongseok Baek <codetemplate@hanmail.net>
2024-10-08 15:05:41 +0900

    enable markdown-preview.lua

--- .config/lazy-nvim/lua/plugins/markdown-preview.lua
+++ .config/lazy-nvim/lua/plugins/markdown-preview.lua
@@ -6,6 +6,15 @@
 -- Preview Markdown in your modern browser with synchronised scrolling and flexible configuration
 return {
   "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  -- build = function()
+  --   vim.fn["mkdp#util#install"]()
+  -- end,
   keys = {
     {
       "<leader>mp",
