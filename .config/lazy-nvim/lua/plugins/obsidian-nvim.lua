8df84f5b0d9541561270a03e000b2a8ebf6ef04d
Myeongseok Baek <codetemplate@hanmail.net>
2024-10-15 16:41:11 +0900

    disable default keymap

--- .config/lazy-nvim/lua/plugins/obsidian-nvim.lua
+++ .config/lazy-nvim/lua/plugins/obsidian-nvim.lua
@@ -23,6 +23,7 @@ return {
         path = "/Users/msbaek/DocumentsLocal/obsidian/ctemplate",
       },
     },
+    use_default_keymaps = false,
     -- Optional, if you keep notes in a specific subdirectory of your vault.
     -- notes_subdir = "notes",
     -- completion = {
@@ -35,29 +36,30 @@ return {
     -- },
     -- -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
     -- -- way then set 'mappings = {}'.
-    -- mappings = {
-    --   -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-    --   ["gf"] = {
-    --     action = function()
-    --       return require("obsidian").util.gf_passthrough()
-    --     end,
-    --     opts = { noremap = false, expr = true, buffer = true },
-    --   },
-    --   -- Toggle check-boxes.
-    --   ["<leader>ch"] = {
-    --     action = function()
-    --       return require("obsidian").util.toggle_checkbox()
-    --     end,
-    --     opts = { buffer = true },
-    --   },
-    -- Smart action depending on context, either follow link or toggle checkbox.
-    -- ["<cr>"] = {
-    --   action = function()
-    --     return require("obsidian").util.smart_action()
-    --   end,
-    --   opts = { buffer = true, expr = true },
-    -- },
-    -- },
+    mappings = {
+      --   -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ["<leader>ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      -- ["<cr>"] = false,
+      -- ["<cr>"] = {
+      --   action = function()
+      --     return require("obsidian").util.smart_action()
+      --   end,
+      --   opts = { buffer = true, expr = true },
+      -- },
+    },
 
     -- Where to put new notes. Valid options are
     --  * "current_dir" - put new notes in same directory as the current buffer.
