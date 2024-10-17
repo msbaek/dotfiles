4a60f2113b9961c6c94ee64ac44fda6cddc6942c
Myeongseok Baek <codetemplate@hanmail.net>
2024-10-17 09:34:04 +0900

    change wezterm font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "dracula"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 18
+config.font_size = 16
 
 config.enable_tab_bar = false
 
