d4033ed95081535bd9d270cf195ccdc34d27a0db
Myeongseok Baek <codetemplate@hanmail.net>
2024-11-19 18:13:11 +0900

    turn auto save off

--- .config/lazy-nvim/lua/plugins/auto-save.lua
+++ .config/lazy-nvim/lua/plugins/auto-save.lua
@@ -14,6 +14,7 @@ return {
     cmd = "ASToggle", -- optional for lazy loading on command
     event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {
+      enabled = false,
       -- your config goes here
       -- or just leave it empty :)
     },
