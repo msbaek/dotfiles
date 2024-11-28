309c3299a625d3c562ff2634b4e9b7a150a5176e
Myeongseok Baek <codetemplate@hanmail.net>
2024-11-28 15:18:58 +0900

    add image path converting in hammperspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -25,6 +25,47 @@ function input_eng()
 	-- end
 end
 
+-- 이미지 경로 변환을 위한 함수
+function convertImagePath()
+	local clipboard = hs.pasteboard.getContents()
+
+	-- Pasted image로 시작하는지 확인
+	if string.match(clipboard, "^Pasted image") then
+		local converted = "![](ATTACHMENTS/" .. string.gsub(clipboard, " ", "%%20") .. ")"
+		hs.pasteboard.setContents(converted)
+
+		-- 성공 알림 표시
+		hs.notify
+			.new({
+				title = "변환 완료",
+				informativeText = "마크다운 이미지 경로가 클립보드에 복사되었습니다.",
+				soundName = "Purr", -- 맥 알림음 추가
+			})
+			:send()
+	else
+		-- 잘못된 형식일 경우 경고 알림
+		hs.notify
+			.new({
+				title = "변환 실패",
+				informativeText = "클립보드의 내용이 'Pasted image'로 시작하지 않습니다.",
+				soundName = "Basso",
+			})
+			:send()
+	end
+end
+
+-- Command + Shift + V 단축키 설정
+hs.hotkey.bind({ "cmd", "shift" }, "V", convertImagePath)
+
+-- hs.hotkey.bind({ "cmd", "shift" }, "V", function()
+-- 	local clipboard = hs.pasteboard.getContents()
+-- 	local converted = "![](ATTACHMENTS/" .. string.gsub(clipboard, " ", "%%20") .. ")"
+-- 	hs.pasteboard.setContents(converted)
+-- 	hs.notify
+-- 		.new({ title = "변환 완료", informativeText = "변환된 텍스트가 클립보드에 복사되었습니다." })
+-- 		:send()
+-- end)
+
 -- hs.hotkey.bind({}, 'tab', input_eng)
 
 esc_bind = hs.hotkey.new({}, "escape", convert_to_eng_with_esc):enable()
