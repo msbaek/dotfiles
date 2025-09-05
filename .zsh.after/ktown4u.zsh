3a9299a17797cdd08e9af280612beb13788d3b74
Myeongseok Baek <codetemplate@hanmail.net>
2025-09-06 08:19:55 +0900

    remove to-zeppelin

--- .zsh.after/ktown4u.zsh
+++ .zsh.after/ktown4u.zsh
@@ -1,5 +1,2 @@
 # Ktown4u 관련 환경변수 로드
 [[ -f ~/.env.ktown4u ]] && source ~/.env.ktown4u
-
-# SSH 접속 alias (TODO: SSH config로 이동 예정)
-lias to-zeppelin='ssh -i ~/DocumentsLocal/hminter-VPN/hmmall-keypair.pem ec2-user@172.16.0.175'
