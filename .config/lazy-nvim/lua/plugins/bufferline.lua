214ac9ea8a088462a3baf0250b3ec5fd08a8f08e
Myeongseok Baek <codetemplate@hanmail.net>
2025-09-15 15:32:48 +0900

    2025-09-15 15:32:42 bufferline works

---
+++ .config/lazy-nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,70 @@
+return {
+  -- Disable LazyVim's default colorscheme integration with bufferline
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        bufferline = false, -- Disable bufferline integration to prevent the error
+      },
+    },
+  },
+  -- Our own bufferline configuration
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    },
+    opts = {
+      options = {
+        close_command = function(n)
+          require("snacks").bufdelete(n)
+        end,
+        right_mouse_command = function(n)
+          require("snacks").bufdelete(n)
+        end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config").icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+        -- Custom colors without catppuccin integration
+        separator_style = "slant",
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(require("bufferline.api").nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
+}
