f9bcbefd66b7da1d4f94ecc01580aede585a0fb8
Myeongseok Baek <codetemplate@hanmail.net>
2025-11-13 11:19:51 +0900

    light theme

---
+++ .config/lazy-nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+-- I manually created this file
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+      colorscheme = "catppuccin-latte",
+      -- colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "eldritch",
+      -- colorscheme = "catppuccin-frappe",
+      -- colorscheme = "Duskfox",
+      -- colorscheme = "Nightfox",
+      -- colorscheme = "Carbonfox",
+      -- colorscheme = "gruvbox",
+    },
+  },
+}
