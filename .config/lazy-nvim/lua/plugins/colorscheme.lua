ef6af0956a0af13cd6dd87b1a0e60b755bab951e
Myeongseok Baek <codetemplate@hanmail.net>
2025-11-13 11:55:52 +0900

    github theme 적용

--- .config/lazy-nvim/lua/plugins/colorscheme.lua
+++ .config/lazy-nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,34 @@
 -- I manually created this file
 return {
+  -- GitHub Theme
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = false,
+          terminal_colors = true,
+          dim_inactive = false,
+          module_default = true,
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-      colorscheme = "catppuccin-latte",
+      -- GitHub Light theme
+      colorscheme = "github_light",
+      -- Previous: catppuccin-latte
+      -- colorscheme = "catppuccin-latte",
       -- colorscheme = "catppuccin-macchiato",
       -- colorscheme = "catppuccin-mocha",
       -- colorscheme = "catppuccin",
